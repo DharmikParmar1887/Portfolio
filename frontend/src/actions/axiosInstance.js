d73a7dfde21dba7ca082cf9d0fcdac373c51a28d
DharmikParmar1887 <dharmikparmar1887@gmail.com>
2024-06-12 00:17:17 +0530

    c20

--- frontend/src/actions/axiosInstance.js
+++ frontend/src/actions/axiosInstance.js
@@ -1,3 +1,16 @@
+// import axios from 'axios';
+
+// const apiURL = process.env.REACT_APP_API_URL;
+
+// const axiosInstance = axios.create({
+//     baseURL: apiURL,
+//     withCredentials: true,
+//     headers: {
+//         "Content-Type": "application/json",
+//     },
+// });
+
+// export default axiosInstance;
 
 
 import axios from 'axios';
@@ -10,6 +23,17 @@ const axiosInstance = axios.create({
     headers: {
         "Content-Type": "application/json",
     },
+});
+
+// Intercept request to add token
+axiosInstance.interceptors.request.use((config) => {
+    const token = localStorage.getItem('token');
+    if (token) {
+        config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+}, (error) => {
+    return Promise.reject(error);
 });
 
 export default axiosInstance;
