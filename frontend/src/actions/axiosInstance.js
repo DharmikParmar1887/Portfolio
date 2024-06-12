fab87ed9d999d3a6e3b4e8c9f3956e998d24e5a5
DharmikParmar1887 <dharmikparmar1887@gmail.com>
2024-06-12 09:51:27 +0530

    c24

--- frontend/src/actions/axiosInstance.js
+++ frontend/src/actions/axiosInstance.js
@@ -1,3 +1,18 @@
+// // import axios from 'axios';
+
+// // const apiURL = process.env.REACT_APP_API_URL;
+
+// // const axiosInstance = axios.create({
+// //     baseURL: apiURL,
+// //     withCredentials: true,
+// //     headers: {
+// //         "Content-Type": "application/json",
+// //     },
+// // });
+
+// // export default axiosInstance;
+
+
 // import axios from 'axios';
 
 // const apiURL = process.env.REACT_APP_API_URL;
@@ -10,22 +25,28 @@
 //     },
 // });
 
+// // Intercept request to add token
+// axiosInstance.interceptors.request.use((config) => {
+//     const token = localStorage.getItem('token');
+//     if (token) {
+//         config.headers.Authorization = `Bearer ${token}`;
+//     }
+//     return config;
+// }, (error) => {
+//     return Promise.reject(error);
+// });
+
 // export default axiosInstance;
 
 
+// axiosInstance.js
 import axios from 'axios';
 
-const apiURL = process.env.REACT_APP_API_URL;
-
 const axiosInstance = axios.create({
-    baseURL: apiURL,
-    withCredentials: true,
-    headers: {
-        "Content-Type": "application/json",
-    },
+    baseURL: 'https://portfolio-i6fi.onrender.com',
 });
 
-// Intercept request to add token
+// Add a request interceptor to include the token in headers
 axiosInstance.interceptors.request.use((config) => {
     const token = localStorage.getItem('token');
     if (token) {
